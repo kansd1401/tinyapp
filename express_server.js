8a553681abfd02dfa72a1933c8673b8ba4dd26a5
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2019-10-29 19:53:28 +0000

    Delete key now works as intended

--- express_server.js
+++ express_server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
