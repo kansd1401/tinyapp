d6090197bd78a792e3da78a773be9181e5ecb611
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2019-10-30 00:01:50 +0000

    Added cookies and logged in and logout features(basic)

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
@@ -13,12 +16,22 @@ const urlDatabase = {
 app.post("/urls", (req, res) => {
   let randomString = generateRandomString()
   urlDatabase[randomString] = req.body.longURL;
-  let templateVars = { shortURL: randomString, longURL: urlDatabase[randomString] };
+  let templateVars = { shortURL: randomString, longURL: urlDatabase[randomString], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls")
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username',req.body.username)
+  res.redirect("/urls")
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
   res.redirect("/urls")
 });
 
@@ -37,22 +50,23 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase ,username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] ,username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("urls/new");
 });
 
 app.listen(PORT, () => {
